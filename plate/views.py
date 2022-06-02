107a760aee9266b48c5d915b9b554d83ff3cd49f
woong <dndvud123@naver.com>
2022-06-02 22:42:31 +0900

    modify index.html, and make class Post

--- plate/views.py
+++ plate/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
 from django.urls import reverse
+from django.views.generic import ListView
 from allauth.account.views import PasswordChangeView
+from plate.models import Review
 
 # Create your views here.
-def index(request):
-    return render(request, 'plate/index.html')
+class IndexListView(ListView):
+    model = Review
+    template_name = "plate/index.html"
+    context_object_name = "reviews"
+    paginate_by = 4
+    ordering = ["-dt_created"]
 
 class CustomPasswordChangeView(PasswordChangeView):
     def get_success_url(self):
         return reverse("index")
+    
