b0e7cda9a4c4382b12a9c28e8b999df16aea2884
woong <dndvud123@naver.com>
2022-06-03 00:13:16 +0900

    add review_detail

--- plate/urls.py
+++ plate/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexListView.as_view(), name="index"),
+    path(
+        "reviews/<int:review_id>",
+        views.ReviewDetail.as_view(),
+        name="review-detail"
+    )
 ]
--- plate/views.py
+++ plate/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from allauth.account.views import PasswordChangeView
 from plate.models import Review
 
@@ -10,6 +10,11 @@ class IndexListView(ListView):
     context_object_name = "reviews"
     paginate_by = 4
     ordering = ["-dt_created"]
+    
+class ReviewDetail(DetailView):
+    model = Review
+    template_name = "plate/review_detail.html"
+    pk_url_kwarg = "review_id"
 
 class CustomPasswordChangeView(PasswordChangeView):
     def get_success_url(self):
